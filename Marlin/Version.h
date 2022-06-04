8de09ab35c3501caa3d1b85ad37dc6e1468d3825
paulmreese <paul.m.reese@gmail.com>
2022-06-04 13:07:10 -0400

    Add custom Polyformer menu

--- Marlin/Version.h
+++ Marlin/Version.h
@@ -28,7 +28,7 @@
 /**
  * Marlin release version identifier
  */
-//#define SHORT_BUILD_VERSION "bugfix-2.0.x"
+//#define SHORT_BUILD_VERSION "Polyformer 0.89"
 
 /**
  * Verbose version identifier which should contain a reference to the location
@@ -41,12 +41,12 @@
  * here we define this default string as the date where the latest release
  * version was tagged.
  */
-//#define STRING_DISTRIBUTION_DATE "2022-06-03"
+//#define STRING_DISTRIBUTION_DATE "2022-06-04"
 
 /**
  * Defines a generic printer name to be output to the LCD after booting Marlin.
  */
-//#define MACHINE_NAME "3D Printer"
+//#define MACHINE_NAME "Polyformer"
 
 /**
  * The SOURCE_CODE_URL is the location where users will find the Marlin Source
